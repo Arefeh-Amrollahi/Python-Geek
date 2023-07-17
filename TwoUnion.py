901b7ba40146271b539fc7d0390716694140f83e
Arefeh Amrollahi <95327529+Arefeh-Amrollahi@users.noreply.github.com>
2023-07-17 13:29:47 -0700

    Union

--- TwoUnion.py
+++ TwoUnion.py
@@ -1,23 +1,3 @@
-'''
-Given two arrays a[] and b[] of size n and m respectively. The task is to find the number of elements in the union between these two arrays.
-
-Union of the two arrays can be defined as the set containing distinct elements from both the arrays. If there are repetitions, then only one occurrence of element should be printed in the union.
-
-Note : Elements are not necessarily distinct.
-
-Example 1:
-
-Input:
-5 3
-1 2 3 4 5
-1 2 3
-Output: 
-5
-Explanation: 
-1, 2, 3, 4 and 5 are the
-elements which comes in the union set
-of both arrays. So count is 5.
-'''
 # Python program for the union of two arrays using Set
 def getUnion(a, n, b, m):
 
@@ -61,4 +41,3 @@ n2 = len(b)
 getUnion(a, n1, b, n2)
 
 # This code is contributed by Aarti_Rathi
-LookupError
