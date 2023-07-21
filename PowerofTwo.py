7c4a713319827dd8c5f05ca0c268a41632566721
Arefeh-Amrollahi <95327529+Arefeh-Amrollahi@users.noreply.github.com>
2023-07-21 14:32:12 -0700

    PowerofTwo

--- PowerofTwo.py
+++ PowerofTwo.py
@@ -1,30 +1,31 @@
-'''. 
-
-All power of two numbers have only one bit set. So count the no. of set bits and if you get 1 then number is a power of 2. Please see Count set bits in an integer for counting set bits.
-4. If we subtract a power of 2 numbers by 1 then all unset bits after the only set bit become set; and the set bit become unset.
-For example for 4 ( 100) and 16(10000), we get following after subtracting 1 
-3 –> 011 
-15 –> 01111
-So, if a number n is a power of 2 then bitwise & of n and n-1 will be zero. We can say n is a power of 2 or not based on value of n&(n-1). The expression n&(n-1) will not work when n is 0. To handle this case also, our expression will become n& (!n&(n-1)) (thanks to https://www.geeksforgeeks.org/program-to-find-whether-a-no-is-power-of-two/Mohammad for adding this case). 
-Below is the implementation of this method.
 '''
-# Python program to check if given
-# number is power of 2 or not
+If x is 2:
+math.log2(2) = 1, which is an integer.
+math.log2(2) % 1 will be equal to 0 because 1 divided by 1 is 1 with no remainder.
+So, math.log2(2) % 1 == 0 will be True.
+
+If x is 8:
+math.log2(8) = 3, which is an integer.
+math.log2(8) % 1 will be equal to 0 because 3 divided by 1 is 3 with no remainder.
+So, math.log2(8) % 1 == 0 will be True.
+
+If x is 5:
+math.log2(5) = 2.321928094887362, which is not an integer.
+math.log2(5) % 1 will be a non-zero decimal value because there is a remainder when 2.321928094887362 is divided by 1.
+So, math.log2(5) % 1 == 0 will be False
+'''
 
 # Function to check if x is power of 2
+import math
+
 def isPowerOfTwo (x):
+    if x < 1:
+        return False
+    return math.log2(x) % 1 == 0
+        
+if __name__ == "__main__":
+    # Function calls
+	print("Yes" if isPowerOfTwo(31) else "No")
 
-	# First x in the below expression
-	# is for the case when x is 0
-	return (x and (not(x & (x - 1))) )
 
-# Driver code
-if(isPowerOfTwo(31)):
-	print('Yes')
-else:
-	print('No')
-	
-if(isPowerOfTwo(64)):
-	print('Yes')
-else:
-	print('No')
+    
