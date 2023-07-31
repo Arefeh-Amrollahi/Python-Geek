d31c79b16c7f7e1dc4b71f28fde246609ed327e4
Arefeh-Amrollahi <95327529+Arefeh-Amrollahi@users.noreply.github.com>
2023-07-31 15:17:21 -0700

    TwoArrayEqual

--- EqualTwoArray.py
+++ EqualTwoArray.py
@@ -1,11 +1,36 @@
-from collections import Counter
 
-def arrays_equal(a, b):
-	return Counter(a) == Counter(b)
+# arr2[0..m-1] contain same elements.
+def areEqual(arr1, arr2, N, M):
 
-a = [3, 2, 1, 3, 2, 1]
-b = [1, 2, 3, 1, 2, 3]
-c = [4, 5, 6, 4, 5, 6]
+	# If lengths of array are not
+	# equal means array are not equal
+	if (N != M):
+		return False
+
+	# Sort both arrays
+	arr1.sort()
+	arr2.sort()
+
+	# Linearly compare elements
+	for i in range(0, N):
+		if (arr1[i] != arr2[i]):
+			return False
+
+	# If all elements were same.
+	return True
+
+
+# Driver Code
+if __name__ == "__main__":
+	arr1 = [3, 5, 2, 5, 2]
+	arr2 = [2, 3, 5, 5, 2]
+	n = len(arr1)
+	m = len(arr2)
+
+	if (areEqual(arr1, arr2, n, m)):
+		print("Yes")
+	else:
+		print("No")
+
  
-print(arrays_equal(a, b))  # True
-print(arrays_equal(a,b))
+ 
