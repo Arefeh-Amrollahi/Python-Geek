be8ae82e331b22d24530d6591d13a2f585cefc55
Arefeh-Amrollahi <95327529+Arefeh-Amrollahi@users.noreply.github.com>
2023-08-21 14:30:02 -0700

    factorial

---
+++ SortArray.py
@@ -0,0 +1,14 @@
+
+
+
+def sort012(arr,n):
+        # code here
+        arr.sort()
+        return arr
+
+if __name__ == "__main__":
+    arr = [0, 1, 1, 0, 1, 2, 1, 2, 0, 0, 0, 1]
+    n = len(arr)
+ 
+    sort012(arr, n)
+    print(arr)
