defd77939b350d9665fe98e6b925f7af0005771b
Arefeh-Amrollahi <95327529+Arefeh-Amrollahi@users.noreply.github.com>
2023-11-24 15:42:11 -0800

    sort

--- SortArray.py
+++ SortArray.py
@@ -1,12 +1,31 @@
+# 
+# Given an array of size N containing only 0s, 1s, and 2s; sort the array in ascending order.
 
-def sort012(arr,n):
-        # code here
-        arr.sort()
-        return arr
 
-if __name__ == "__main__":
-    arr = [0, 1, 1, 0, 1, 2, 1, 2, 0, 0, 0, 1]
-    n = len(arr)
- 
-    sort012(arr, n)
-    print(arr)
+# Example 1:
+
+# Input: 
+# N = 5
+# arr[]= {0 2 1 2 0}
+# Output:
+# 0 0 1 2 2
+# Explanation:
+# 0s 1s and 2s are segregated 
+# into ascending order.
+
+class Solution:
+    def sort012(self, arr, n):
+        # Sort the array in ascending order and return a copy
+        return sorted(arr)
+
+# Example usage:
+arr1 = [0, 2, 1, 2, 0]
+n1 = len(arr1)
+solution = Solution()
+sorted_arr1 = solution.sort012(arr1, n1)
+print(sorted_arr1)  # Output: [0, 0, 1, 2, 2]
+
+arr2 = [0, 1, 0]
+n2 = len(arr2)
+sorted_arr2 = solution.sort012(arr2, n2)
+print(sorted_arr2)  # Output: [0, 0, 1]
