e85f19e4b0178a06f123202426331437aaacd37a
Arefeh-Amrollahi <95327529+Arefeh-Amrollahi@users.noreply.github.com>
2024-05-17 20:55:39 -0700

    search

--- Binary-search.py
+++ Binary-search.py
@@ -41,38 +41,83 @@ Explanation: 4 appears at index 3.
 # Search.
 
 
-# It returns location of x in given array arr
-def binarySearch(arr, l, r, x):
+# # It returns location of x in given array arr
+# def binarySearch(arr, l, r, x):
 
-	while l <= r:
+# 	while l <= r:
 
-		mid = l + (r - l) // 2
+# 		mid = l + (r - l) // 2
 
-		# Check if x is present at mid
-		if arr[mid] == x:
-			return mid
+# 		# Check if x is present at mid
+# 		if arr[mid] == x:
+# 			return mid
 
-		# If x is greater, ignore left half
-		elif arr[mid] < x:
-			l = mid + 1
+# 		# If x is greater, ignore left half
+# 		elif arr[mid] < x:
+# 			l = mid + 1
 
-		# If x is smaller, ignore right half
-		else:
-			r = mid - 1
+# 		# If x is smaller, ignore right half
+# 		else:
+# 			r = mid - 1
 
-	# If we reach here, then the element
-	# was not present
-	return -1
+# 	# If we reach here, then the element
+# 	# was not present
+# 	return -1
+
+
+# # Driver Code
+# if __name__ == '__main__':
+# 	arr = [2, 3, 4, 10, 40]
+# 	x = 10
+
+# 	# Function call
+# 	result = binarySearch(arr, 0, len(arr)-1, x)
+# 	if result != -1:
+# 		print("Element is present at index", result)
+# 	else:
+# 		print("Element is not present in array")
+
+# Python3 Program for recursive binary search.
+
+
+# Returns index of x in arr if present, else -1
+def binarySearch(arr, low, high, x):
+
+    # Check base case
+    if high >= low:
+
+        mid = low + (high - low) // 2
+
+        # If element is present at the middle itself
+        if arr[mid] == x:
+            return mid
+
+        # If element is smaller than mid, then it
+        # can only be present in left subarray
+        elif arr[mid] > x:
+            return binarySearch(arr, low, mid-1, x)
+
+        # Else the element can only be present
+        # in right subarray
+        else:
+            return binarySearch(arr, mid + 1, high, x)
+
+    # Element is not present in the array
+    else:
+        return -1
 
 
 # Driver Code
 if __name__ == '__main__':
-	arr = [2, 3, 4, 10, 40]
-	x = 10
+    arr = [2, 3, 4, 10, 40]
+    x = 10
+    
+    # Function call
+    result = binarySearch(arr, 0, len(arr)-1, x)
+    
+    if result != -1:
+        print("Element is present at index", result)
+    else:
+        print("Element is not present in array")
 
-	# Function call
-	result = binarySearch(arr, 0, len(arr)-1, x)
-	if result != -1:
-		print("Element is present at index", result)
-	else:
-		print("Element is not present in array")
+ 
